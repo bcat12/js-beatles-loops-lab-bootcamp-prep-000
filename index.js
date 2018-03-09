53a6c3a1a52b7abb2445207c468c75779cb9361e
Brian Caterfino <bcaterfino@gmail.com>
2018-03-09 18:02:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function theBeatlesPlay(musician, instreument){
   var empty = [];
   for (var i=0; i< musician; i++)
    empty.push(musicians[i]+ "plays" + instrument[i])
-  return empty;
+return empty;
 }
